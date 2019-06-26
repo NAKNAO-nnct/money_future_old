c6c4b3cd3dde09b6e2225bef841e518456e4bea2
NAKNAO-nnct <totoro.windows@outlook.com>
2019-06-27 00:20:01 +0900

    消すボタンの実装

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
+// vue
+var vue_cons = [0];
 
+// storage
+var database_key = "money_future";
+var user_data = {
+    items: [
+        {
+            id: 1, name: '家を買う', money: '-100', date: '2019/06/19', type: 'out'
+        },
+        {
+            id: 2, name: 'BD買う', money: '-2000', date: '2019/03/09', type: 'out'
+        },
+        {
+            id: 3, name: '初田家買収', money: '-30', date: '2019/09/12', type: 'out'
+        },
+        {
+            id: 4, name: 'Apple売却', money: '30000', date: '2019/11/12', type: 'in'
+        }
+    ],
+    have: {
+        balance: 500
+    }
+};
 
-document.getElementById("body").onchange = function () {
+window.onchange = function(){
     display_money();
 }
 
-document.getElementById("body").onload = function () {
-    display_money();
-}
+                
 
 function display_money() {
     var balance = parseInt(document.getElementById("balance").innerHTML);
@@ -24,14 +45,8 @@ function display_money() {
 // 反映
 function check_button(button) {
     console.log(button.parentNode.parentNode.parentNode.parentNode.style[0] == "opacity");
-    if (button.parentNode.parentNode.parentNode.parentNode.style[0] == "opacity") {
-        button.parentNode.parentNode.parentNode.parentNode.style = "";
-    }
-    else {
-        button.parentNode.parentNode.parentNode.parentNode.style = "opacity: 0.3;";
-    }
-
-
+    
+    button.parentNode.parentNode.parentNode.classList.toggle("card-disable");
 }
 // var check_button = document.getElementsByClassName("check-botton");
 // for (let i = 0; i < check_button.length; i++) {
@@ -40,28 +55,68 @@ function check_button(button) {
 //     }
 // }
 
-// delete
-function delete_card() {
+// Web storage
+// init
+(function init_db(){
+    // localstorageにデータがなければデータを挿入
+    if(localStorage[database_key] != undefined){
+        user_data = JSON.parse(localStorage[database_key]);
+    }else{
+        localStorage[database_key] = JSON.stringify(user_data);
+    }
+    console.log(user_data);
+    // vue
+    vue_cons[0] = new Vue({
+        el: "#app",
+        data: user_data,
+        methods: {
+            update: function(){
+                this.$forceUpdate();
+                display_money();
+            }
+        },
+    });
 
+
+    vue_cons[1] = new Vue({
+        el: '#balance_area',
+        data: user_data,
+        methods: {
+            update: function () {
+                const balance = parseInt(document.getElementById("balance_money").value);
+                if(!isNaN(balance)){
+                    user_data["have"].balance = balance;
+                    this.$forceUpdate();
+                    display_money();
+                }
+            }
+        }
+    })
+    display_money();
+}());
+
+// insert
+function insert_db(insert_data){}
+
+// get
+function get_data(){
+    console.log(localStorage[database_key]);
+};
+
+// delete
+function delete_data(id) {
+    var n;
+    for(let i=0; i<user_data["items"].length; i++){
+        if(user_data["items"][i]["id"] == id){
+            delete user_data["items"][i];
+            n = i;
+        }
+    }
+    
+    user_data["items"] = user_data["items"].filter(n => n !== 1);
+    console.log(user_data["items"]);
+    vue_cons[0].update();
 }
 
-// vue
-new Vue({
-    el: "#app",
-    data: {
-        items: [
-            {
-                name: '家を買う', money: '-100', date: '2019/06/19', type: 'out'
-            },
-            {
-                name: 'BD買う', money: '-2000', date: '2019/03/09', type: 'out'
-            },
-            {
-                name: '初田家買収', money: '-30', date: '2019/09/12', type: 'out'
-            },
-            {
-                name: 'Apple売却', money: '30000', date: '2019/11/12', type: 'in'
-            }
-        ]
-    }
-});
+// 
+
